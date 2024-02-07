c6b0e59fead776ac8dec2caa7bf8602772f45b2d
joobin <ss>
2024-02-07 18:55:49 +0330

    efficient bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,12 +1,19 @@
 A=[23,-23,5,1,5,6,2,0,-9,-6]
+A=[-23, -9, -6, 0, 1, 2, 5, 5, 6, 23]
 kiri_face=0
 for i in range(len(A)):
+      has_at_least_bubble=False
       for j in range(len(A)-1,i,-1):
+            kiri_face+=1
             if A[j]<A[j-1]:
+                  has_at_least_bubble=True
                   c=A[j]
                   A[j]=A[j-1]
                   A[j-1]=c
-                  kiri_face+=1
+      if has_at_least_bubble is not True:
+            break
+      
+      
       
 
 print(A)
---
+++ sort/bucket/bucket_sort.py
@@ -0,0 +1,8 @@
+""" all number has same feature both is two digit """
+A=[34,5,99,11,44,99,33,44]
+
+
+
+#bucket definition for saving and classifying data ... array of bucket index is sorted naturally
+
+B=
